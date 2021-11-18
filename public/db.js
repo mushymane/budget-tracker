f2e9d5ed93b71e995b645f192a703afdefa09ac3
Gabriel Lantin <mushymanee@gmail.com>
2021-11-18 01:40:23 -0800

    implemented service worker and fixed readwrite typo, still need to fix icon thing

--- public/db.js
+++ public/db.js
@@ -20,13 +20,13 @@ request.onerror = function(event) {
 }
 
 function saveRecord(record) {
-    const transaction = db.transaction(["pending"], "readWrite");
+    const transaction = db.transaction(["pending"], "readwrite");
     const store = transaction.objectStore("pending");
     store.add(record);
 }
 
 function checkDatabase() {
-    const transaction = db.transaction(["pending"], "readWrite");
+    const transaction = db.transaction(["pending"], "readwrite");
     const store = transaction.objectStore("pending");
     const getAll = store.getAll();
 
@@ -42,7 +42,7 @@ function checkDatabase() {
             })
             .then(response => response.json())
             .then(() => {
-                const transaction = db.transaction(["pending"], "readWrite");
+                const transaction = db.transaction(["pending"], "readwrite");
                 const store = transaction.objectStore("pending");
                 store.clear();
             })
---
+++ public/service-worker.js
@@ -0,0 +1,80 @@
+const FILES_TO_CACHE = [
+    '/',
+    '/styles.css',
+    '/index.html',
+    '/dist/db.bundle.js',
+    '/dist/index.bundle.js',
+    '/dist/manifest.json',
+    '/dist/icon_192x192.png',
+    '/dist/icon_512x512.png',
+    "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+    "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+];
+
+const STATIC_CACHE = "static-cache-v1";
+const RUNTIME_CACHE = "runtime-cache";
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches
+            .open(STATIC_CACHE)
+            .then(cache => cache.addAll(FILES_TO_CACHE))
+            .then(() => self.skipWaiting())
+    );
+})
+
+self.addEventListener("activate", event => {
+    const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
+    event.waitUntil(
+        caches
+            .keys()
+            .then(cacheNames => {
+                return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+            })
+            .then(cachesToDelete => {
+                return Promise.all(cachesToDelete.map(cacheToDelete => {
+                    return caches.delete(cacheToDelete);
+                }))
+            })
+            .then(() => self.clients.claim())
+    )
+})
+
+self.addEventListener("fetch", event => {
+    if (event.request.method !== 'GET' || !event.request.url.startsWith(self.location.origin)) {
+        event.respondWith(fetch(event.request));
+        return;
+    }
+
+    if (event.request.url.includes('/api')) {
+        event.respondWith(
+            caches.open(RUNTIME_CACHE).then(cache => {
+                return fetch(event.request).then(response => {
+                    if (response.status === 200) {
+                        cache.put(event.request.url, response.clone());
+                    }
+                    return response;
+                }).catch(err => {
+                    return caches.match(event.request);
+                })
+            }).catch(err => console.log(err))
+        );
+        return;
+    }
+
+    event.respondWith(
+        caches.match(event.request).then(cachedResponse => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+
+            return caches.open(RUNTIME_CACHE).then(cache => {
+                return fetch(event.request).then(response => {
+                    return cache.put(event.request, response.clone()).then(() => {
+                        return response;
+                    })
+                })
+            })
+        })
+    )
+})
